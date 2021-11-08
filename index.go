30d2268555757fec805142576d7a498ebffbbb9c
AldiPrayogi <aldi_prayogi@yahoo.com>
2021-11-08 15:05:48 +0700

    edit

--- index.go
+++ index.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,16 +14,16 @@ import (
 const (
 	DB_USER     = "postgres"
 	DB_PASSWORD = "AldiPC"
-	DB_NAME     = "movies"
+	DB_NAME     = "postgres"
 )
 
 func setupDB() *sql.DB {
-	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disabled", DB_NAME, DB_PASSWORD, DB_USER)
+	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable", DB_NAME, DB_PASSWORD, DB_USER)
 	db, err := sql.Open("postgres", dbinfo)
 
 	checkErr(err)
 
-	return DB
+	return db
 }
 
 type Movie struct {
@@ -61,4 +62,111 @@ func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Getting movies...")
+
+	rows, err := db.Query("SELECT * FROM movies")
+
+	checkErr(err)
+
+	var movies []Movie
+
+	for rows.Next() {
+		var id int
+		var movieID string
+		var movieName string
+
+		err = rows.Scan(&id, &movieID, &movieName)
+
+		checkErr(err)
+
+		movies = append(movies, Movie{
+			MovieID:   movieID,
+			MovieName: movieName,
+		})
+
+		var response = JsonResponse{Type: "success", Data: movies}
+
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	movieID := r.FormValue("movieid")
+	movieName := r.FormValue("moviename")
+
+	var response = JsonResponse{}
+
+	if movieID == "" || movieName == "" {
+		response = JsonResponse{
+			Type:    "error",
+			Message: "Your are missing movieID or movieName parameter.",
+		}
+	} else {
+		db := setupDB()
+
+		printMessage("Inserting movie into DB")
+
+		fmt.Println("Inserting new movie with ID: " + movieID + " and name: " + movieName)
+
+		var lastInsertID int
+		err := db.QueryRow("INSERT INTO movies(movieID, movieName) VALUES($1, $2) returning id;", movieID, movieName).Scan(&lastInsertID)
+
+		checkErr(err)
+
+		response = JsonResponse{
+			Type:    "success",
+			Message: "The movie has been inserted succesfully",
+		}
+
+		json.NewEncoder(w).Encode(response)
+	}
+}
+
+func deleteMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Deleting all movies...")
+
+	_, err := db.Exec("DELETE FROM movies")
+
+	checkErr(err)
+
+	printMessage("All movies have been deleted successfully!")
+
+	response := JsonResponse{
+		Type:    "success",
+		Message: "All movies have been deleted",
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	movieID := params["movieid"]
+
+	var response = JsonResponse{}
+
+	if movieID == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing movieID parameter."}
+	} else {
+		db := setupDB()
+
+		printMessage("Deleting movie from DB")
+
+		_, err := db.Exec("DELETE FROM movies where movieID = $1", movieID)
+
+		// check errors
+		checkErr(err)
+
+		response = JsonResponse{Type: "success", Message: "The movie has been deleted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
